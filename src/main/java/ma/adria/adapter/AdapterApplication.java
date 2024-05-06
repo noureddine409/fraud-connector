ab05114832f5e8f2c1b876681a83484379e17101
noureddine <noureddinelachgar9@gmail.com>
2024-05-06 08:35:02 +0100

    init project

---
+++ src/main/java/ma/adria/adapter/AdapterApplication.java
@@ -0,0 +1,13 @@
+package ma.adria.adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdapterApplication.class, args);
+    }
+
+}
